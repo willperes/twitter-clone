c4b42bbd42d658641a6544af0382016b1be77b30
willperes <thewidocs@gmail.com>
2023-10-28 12:05:13 -0300

    fix: tsconfig and babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.ts', '.android.ts', '.ts', '.tsx', '.json'],
-        alias: {
-          tests: ['./tests/'],
-          '@': './src',
-        },
-      },
-    ],
-  ],
 };
