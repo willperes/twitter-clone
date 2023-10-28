4be7a1d33a4e6a0a6f18758ba7b6439e1749e970
willperes <thewidocs@gmail.com>
2023-10-28 15:35:21 -0300

    feature: change bundle identifier name

--- android/app/src/main/java/br/com/willperes/twitter/MainActivity.java
+++ android/app/src/main/java/br/com/willperes/twitter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.twitter;
+package br.com.willperes.twitter;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/br/com/willperes/twitter/MainApplication.java
+++ android/app/src/main/java/br/com/willperes/twitter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.twitter;
+package br.com.willperes.twitter;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
